85890277a0e3bf2bf3723b3d5a64212a9fa00163
yxnzhe <yanzhe2003@gmail.com>
2022-09-11 19:13:04 +0800

    babylonjs

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', function() {
+    var canvas = document.getElementById('canvas');
+    var engine = new BABYLON.Engine(canvas, true);
+
+    var createScene = function() {
+        var scene = new BABYLON.Scene(engine);
+        scene.clearColour = new BABYLON.Color3.White();
+        var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 0, -10), scene);
+        camera.setTarget(BABYLON.Vector3.Zero());
+        var box = BABYLON.Mesh.CreateBox("Box", 4.0, scene);
+        return scene;
+    }
+
+    var scene = createScene();
+    engine.runRenderLoop(function() {
+        scene.render();
+    })
+});
